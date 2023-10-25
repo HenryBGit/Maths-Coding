8e6a98e06e9e96380dd6410d8cf60120e525af71
HenryBGit <henrybrookland8@gmail.com>
2023-10-25 14:02:18 +1300

    changed the code arouund

--- Algorithms/Something named/Program.cs
+++ Algorithms/Something named/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace Something_named
+﻿using System.ComponentModel.Design;
+
+namespace Something_named
 {
     internal class Program
     {
         static void Main()
         {
+            Console.Write("Please enter in a time in hours (1-12): ");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter in a time in minutes (0-60): ");
+            int minutes = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter in a time in seconds (0-60): ");
+            int seconds = Convert.ToInt32(Console.ReadLine());  
 
         }
     }
